389eecaff9c24521340c4794fe9aab8d01c05954
twinklymeat <twinklymeat8990@gmail.com>
2025-03-15 22:09:32 -0400

    added il support

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ async function fetchLeaderboard() {
             alert('Please select a category for the level!');
             return;
         }
-        url = `https://www.speedrun.com/api/v1/leaderboards/${gameId}/level/${levelOrCategoryId}/category/${categoryOrSubcategoryId}?top=100&embed=players`;
+        url = `https://www.speedrun.com/api/v1/leaderboards/${gameId}/level/${levelOrCategoryId}/${categoryOrSubcategoryId}?top=100&embed=players`;
     }
 
     try {
